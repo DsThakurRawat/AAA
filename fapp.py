19470c13412f003630693d0822d6fc259f56acc2
Divyansh Rawat <divyanshrawatofficial@gmail.com>
2025-06-08 20:31:25 +0530

    Add files via upload

---
+++ fapp.py
@@ -0,0 +1,76 @@
+import json
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+import pickle
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+# Load encoder categories/classes from JSON
+with open("geo_categories.json") as f:
+    geo_categories = json.load(f)
+with open("gender_classes.json") as f:
+    gender_classes = json.load(f)
+
+# Recreate encoders
+onehot_encoder_geo = OneHotEncoder(categories=geo_categories)
+onehot_encoder_geo.fit([[cat] for cat in geo_categories[0]])  # Fit with dummy data
+label_encoder_gender = LabelEncoder()
+label_encoder_gender.fit(gender_classes)
+
+# Load scaler as before
+with open("scaler.pkl", "rb") as file:
+    scaler = pickle.load(file)
+
+# Load model
+model = tf.keras.models.load_model("model.h5")
+
+# Streamlit app
+st.title("customer churn prediction")
+# user input
+geography = st.selectbox("Geography", onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider("Age", 18, 92)
+balance = st.number_input("Balance")
+credit_score = st.number_input("Credit Score")
+estimated_salary = st.number_input("Estimated Salary")
+tenure = st.slider("Tenure", 0, 10)
+num_of_products = st.slider("Number of Products", 1, 4)
+has_cr_card = st.selectbox("Has Credit Card", [0, 1])
+is_active_member = st.selectbox("Is Active Member", [0, 1])
+
+# prepare the input data
+input_data = pd.DataFrame({
+    "CreditScore": [credit_score],
+    "Geography": [geography],
+    "Gender": [gender],
+    "Age": [age],
+    "Tenure": [tenure],
+    "Balance": [balance],
+    "NumOfProducts": [num_of_products],
+    "HasCrCard": [has_cr_card],
+    "IsActiveMember": [is_active_member],
+    "EstimatedSalary": [estimated_salary]
+})
+# Label encode the Gender column before one-hot encoding Geography
+input_data['Gender'] = label_encoder_gender.transform(input_data["Gender"])
+
+# one hot encode "Geography"
+geo_encoded = onehot_encoder_geo.transform(input_data[["Geography"]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(["Geography"]))
+
+# combine one-hot encoded columns with input data
+input_data = pd.concat([input_data.drop("Geography", axis=1), geo_encoded_df], axis=1)
+
+# scale the input data
+input_data_scaled = scaler.transform(input_data)
+
+# make prediction
+prediction = model.predict(input_data_scaled)
+prediction_prob = prediction[0][0]
+
+if prediction_prob > 0.5:
+    st.write("The customer is likely to churn")
+else:
+    st.write("The customer is not likely to churn")
+
